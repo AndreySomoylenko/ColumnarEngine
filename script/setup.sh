60c072b7628a5805c665f2260a6e0d0faa9441ee
Ismail Velidzhanov <iavelidzhanov@edu.hse.ru>
2026-05-16 00:33:16 +0300

    :u:

--- script/setup.sh
+++ script/setup.sh
@@ -3,10 +3,17 @@ set -euo pipefail
 
 apt-get update
 apt-get install -y --no-install-recommends \
-  cmake \
-  g++ \
-  ninja-build \
-  make \
-  ca-certificates
+    software-properties-common \
+    ca-certificates \
+    cmake \
+    ninja-build \
+    make
+
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y --no-install-recommends g++-14 gcc-14
+
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 100
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-14 100
 
 rm -rf /var/lib/apt/lists/*
