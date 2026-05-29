a7a5840c08af83615f93926a74ebad43a85df4b0
Ismail Velidzhanov <iavelidzhanov@edu.hse.ru>
2026-05-29 19:37:32 +0300

    Something

--- src/operations/Aggregations.h
+++ src/operations/Aggregations.h
@@ -181,7 +181,7 @@ void CountDistinct(const std::shared_ptr<Column> &column,
         return;
     }
 
-    answer.reserve(column->Size());
+    // answer.reserve(column->Size());
 
     for (size_t i = 0; i < column->Size(); ++i) {
         if constexpr (detail::StringValue<T>) {
